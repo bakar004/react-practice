3b4ad58cca764d18e74fc0334c97be059a1d6432
bakar004 <abubakerr004@gmail.com>
2026-08-21 20:29:26 +0500

    changed div color

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,6 +1,6 @@
 function Card(props){
     return(
-<div className="bg-amber-300 font-bold p-8">
+<div className="bg-green-300 font-bold p-8">
     
     <h2>{props.title}</h2>
     <p>{props.desc}</p>
